6789667ac9dd3e04e3db64222e435b91834b9b10
WINDOWS\dungb <dungbt2202.forwork@gmail.com>
2024-07-01 17:26:52 +0700

    add controller for test

---
+++ FitAppServer/FitAppServer/Controllers/FCMTestController.cs
@@ -0,0 +1,25 @@
+﻿using FitAppServer.Utils;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitAppServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FCMTestController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> TestPushNotification([FromQuery] string title, [FromQuery] string body, [FromQuery] string token)
+        {
+            try
+            {
+                await FcmUtils.PushNotificationAsync(title, body, token);
+                return Ok("message sent");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+    }
+}
