90e5a9c5d33f75cb37d058213a24a133ebd9f6a7
LiquidRekto <dungliqrekt@gmail.com>
2024-07-03 15:38:11 +0700

    add jwt decode functionality

--- FitAppClient/app/src/main/java/com/example/fitapp/MainActivity.java
+++ FitAppClient/app/src/main/java/com/example/fitapp/MainActivity.java
@@ -7,16 +7,21 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.auth0.android.jwt.Claim;
+import com.auth0.android.jwt.JWT;
 import com.example.fitapp.activity.FCMTestActivity;
 import com.example.fitapp.activity.HomeActivity;
 import com.example.fitapp.activity.LaunchActivity;
 import com.example.fitapp.util.SharedPrefUtils;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Map;
 
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = FCMTestActivity.class.getName();
     SharedPreferences sharedPref;
     //Button fcmSwitch;
     @Override
@@ -40,7 +45,18 @@ public class MainActivity extends AppCompatActivity {
 
         sharedPref = getSharedPreferences("com.example.fitapp", Context.MODE_PRIVATE);
         String token = SharedPrefUtils.retrieveKeyValue(sharedPref, "jwt_token");
+
         if (token != "") {
+            JWT parsedJWT = new JWT(token);
+            // Get all claims as a JMap
+            Map<String, Claim> claims = parsedJWT.getClaims();
+
+            // Build a single string containing all claims (key-value pairs)
+            StringBuilder allClaimsString = new StringBuilder();
+            for (Map.Entry<String, Claim> entry : claims.entrySet()) {
+                allClaimsString.append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
+            }
+            Log.d(TAG, "Claims: " + allClaimsString.toString());
             (new Handler()).postDelayed(this::goToHome, 1000);
         } else {
             (new Handler()).postDelayed(this::goToLaunch, 1000);
