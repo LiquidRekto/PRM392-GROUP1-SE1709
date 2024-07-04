f17e3777dcc07ec230068fd9058ec1e6f12a0548
LiquidRekto <dungliqrekt@gmail.com>
2024-07-04 17:25:42 +0700

    add new schedule classes

---
+++ FitAppServer/FitAppServer/Controllers/ScheduleController.cs
@@ -0,0 +1,45 @@
+﻿using FitAppServer.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitAppServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScheduleController : ControllerBase
+    {
+        [HttpGet]
+        [Route("all")]
+        public async Task<IActionResult> GetAllSchedule()
+        {
+            return Ok(null);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetScheduleById([FromRoute] int id)
+        {
+            return Ok(null);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateSchedule(ScheduleCreateDTO dto)
+        {
+            return Ok(null);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> UpdateSchedule([FromRoute] int id, [FromBody] ScheduleCreateDTO dto)
+        {
+            return Ok(null);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> DeleteSchedule([FromRoute] int id)
+        {
+            return Ok(null);
+        }
+    }
+}
---
+++ FitAppServer/FitAppServer/DTOs/ScheduleCreateDTO.cs
@@ -0,0 +1,13 @@
+﻿using FitAppServer.Models;
+
+namespace FitAppServer.DTOs
+{
+    public class ScheduleCreateDTO
+    {
+        public int TrainerId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public User Trainee { get; set; }
+        public User Trainer { get; set; }
+    }
+}
